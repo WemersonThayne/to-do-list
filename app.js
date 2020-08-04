9b5b3ce9005203bfd072c1e0636208b43a7ed322
Wemerson Thayne Vital Porto <wemerson.vitalporto@ctis.com.br>
2020-08-04 17:23:35 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+
+var count = 1;
+var idEditActual = 0;
+
+function addItem() {
+
+    let item = document.getElementById('box');
+    let list_item = document.getElementById('listItem');
+    if (item.value.trim() !== '' && idEditActual === 0) {
+        // Criar o elemento para adicionar na lista
+        let criarElementoLi = document.createElement('LI');
+        criarElementoLi.appendChild(document.createTextNode(item.value));
+        criarElementoLi.setAttribute("id", count++);
+
+        /** Adiciona no final da LI um X para remover o item  */
+        let spanClose = addRemoveIcon();
+        criarElementoLi.appendChild(spanClose);
+
+        /** Adiciona no final da LI um X para remover o item  */
+        let spanEditar = addEditIcon();
+        criarElementoLi.appendChild(spanEditar);
+
+        /** Adiciona o item na lista */
+        list_item.appendChild(criarElementoLi);
+
+        // Adiciona o evento de click para remover e editar o item 
+        removeItem(spanClose, criarElementoLi);
+        editItem(spanEditar, criarElementoLi);
+        
+        // limpa o input
+        clearInputValue(item);
+    } else if (idEditActual !== 0) {
+
+        document.getElementById('listItem').childNodes.forEach(element => {
+            if (Number(element.id) === idEditActual) {
+                editElement(element, item);
+            }
+        });
+        
+    } else {
+        alert("Por favor, adicione um item");
+        clearInputValue(item);
+    }
+
+}
+
+function addRemoveIcon() {
+    let span = document.createElement('SPAN');
+    span.className = 'close glyphicon glyphicon-remove';
+    return span;
+}
+
+function addEditIcon() {
+    let span = document.createElement('SPAN');
+    span.className = 'editar glyphicon glyphicon-edit';
+    return span;
+}
+
+function removeItem(span, item) {
+    span.onclick = function () {
+        item.remove();
+        this.parentNode.removeChild(this);
+    }
+}
+
+function editItem(span, item) {
+    span.onclick = function () {
+        idEditActual = Number(item.id);
+        document.getElementById('box').value = item.textContent;
+    }
+}
+
+function clearInputValue(input) {
+    input.value = "";
+}
+
+function editElement(element, item) {
+    element.textContent = item.value;
+    let spanClose = addRemoveIcon();
+    let spanEditar = addEditIcon();
+
+    element.appendChild(addRemoveIcon());
+    element.appendChild(addEditIcon());
+
+    removeItem(spanClose, element);
+    editItem(spanEditar, element);
+
+    clearInputValue(item);
+    idEditActual = 0;
+}
